be6c610a5b4c4b341bfce957cd57fd1113ff5d6b
M Ahmed Mushtaq <mmushtaq.bese19seecs@seecs.edu.pk>
2020-09-05 21:25:38 +0500

    all middlewares added

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
-interface color {
-  name: string;
-}
+export * from './errors/bad-request-error';
+export * from './errors/custom-errors';
+export * from './errors/database-connection-error';
+export * from './errors/not-authorized-error';
+export * from './errors/not-found-error';
+export * from './errors/request-validation-error';
 
-const color: color = {
-  name: 'red',
-};
-
-console.log(color);
-
-
-export default color;
+export * from './middlewares/current-user';
+export * from './middlewares/error-handler';
+export * from './middlewares/require-auth';
+export * from './middlewares/validate-request';
