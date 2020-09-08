f071e31fcf4cb4e96ba2c406b1d55c5f9cb1ccf0
M Ahmed Mushtaq <mmushtaq.bese19seecs@seecs.edu.pk>
2020-09-08 17:03:20 +0500

    add console in publisher publish method

--- src/events/base-publisher.ts
+++ src/events/base-publisher.ts
@@ -8,14 +8,14 @@ export abstract class Publisher<T extends Event> {
     this.client = client;
   }
 
-  publish(data: T['data']):Promise<void> {
-      return new Promise((resolve,reject)=>{
-        this.client.publish(this.subject, JSON.stringify(data), (err) => {
-             if(err) return reject(err);
+  publish(data: T['data']): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.client.publish(this.subject, JSON.stringify(data), (err) => {
+        if (err) return reject(err);
 
-             resolve()
-
-          });
-      })
+        console.log('event published successfully');
+        resolve();
+      });
+    });
   }
 }
