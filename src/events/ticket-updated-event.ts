528b9a06da8ed3ae00aca00f10a38bf76f3453c8
M Ahmed Mushtaq <mmushtaq.bese19seecs@seecs.edu.pk>
2020-09-11 07:27:58 +0500

    Add version support in events

--- src/events/ticket-updated-event.ts
+++ src/events/ticket-updated-event.ts
@@ -1,11 +1,12 @@
 import { Subjects } from './subjects';
 
-export interface TicketUpdatedEvent{
-   subject: Subjects.TicketUpdated;
-   data:{
-       id: string,
-       title: string,
-       price: number,
-       userId: string,
-   }
+export interface TicketUpdatedEvent {
+  subject: Subjects.TicketUpdated;
+  data: {
+    id: string;
+    version: number;
+    title: string;
+    price: number;
+    userId: string;
+  };
 }
